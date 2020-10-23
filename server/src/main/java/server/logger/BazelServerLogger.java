5f87e707380f438920b267c0018461a4d049357d
Ben Carlson <benocarlson@gmail.com>
2020-10-23 11:38:31 -0600

    Add logger class

---
+++ server/src/main/java/server/logger/BazelServerLogger.java
@@ -0,0 +1,30 @@
+package server.logger;
+
+
+import java.io.PrintStream;
+
+public class BazelServerLogger {
+
+    private static BazelServerLogger instance;
+
+    private PrintStream logStream;
+
+    private BazelServerLogger(PrintStream stream) {
+        this.logStream = stream;
+    }
+
+    private BazelServerLogger() {
+        this.logStream = System.out;
+    }
+
+    public static BazelServerLogger getLogger() {
+        if (instance == null) {
+            instance = new BazelServerLogger();
+        }
+        return instance;
+    }
+
+    public void log(String line) {
+        logStream.println(line);
+    }
+}
