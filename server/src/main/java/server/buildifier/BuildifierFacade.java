10d18933820abc98e3acb2a121afd1f17ca49b55
josiahsrc <contact@josiahsaunders.com>
2021-01-12 20:00:07 +0000

    install buildifier in container

--- server/src/main/java/server/buildifier/BuildifierFacade.java
+++ server/src/main/java/server/buildifier/BuildifierFacade.java
@@ -1,15 +1,23 @@
 package server.buildifier;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import server.workspace.ExtensionConfig;
 import server.workspace.Workspace;
 
 import java.io.File;
+import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 
 public class BuildifierFacade {
+    private static final String DEFAULT_BUILDIFIER_ENV_NAME = "buildifier";
+
+    private static final Logger logger = LogManager.getLogger(BuildifierFacade.class);
+
     private BuildifierFacade() {
 
     }
@@ -35,7 +43,12 @@ public class BuildifierFacade {
 
         // Try to find the buildifier in the system PATH.
         {
-
+            final Map<String, String> env = System.getenv();
+            if (env.containsKey(DEFAULT_BUILDIFIER_ENV_NAME)) {
+                final String execPath = env.get(DEFAULT_BUILDIFIER_ENV_NAME);
+                final URI execURI = URI.create(execPath);
+                return Paths.get(execURI);
+            }
         }
 
         return null;
