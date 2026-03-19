73a61cda39d11f461216d8773ab024aa24347be7
Claudio Bandera <cbandera@users.noreply.github.com>
2026-03-20 00:36:46 +0100

    build: Bundle extension with esbuild (#561)
    Improves packages size from 5MB to 270kB
    Improves activation time from 260ms to 85ms
    Speeds up build significantly.
    
    Closes #538

--- .vscode-test.js
+++ .vscode-test.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vscode/test-cli");
 
 module.exports = defineConfig({
-  files: "out/test/**/*.test.js",
+  files: "dist/test/**/*.test.js",
   mocha: {
     ui: "bdd",
     timeout: 5000,
